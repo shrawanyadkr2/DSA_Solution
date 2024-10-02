bc47f1ae977dbe125c78f316345a6da40f225816
shrawan kr. yadav <146319317+shrawanyadkr2@users.noreply.github.com>
2024-10-02 23:37:15 +0530

    Create 20-sep-2024-- Swap Two Numbers.cpp

---
+++ Bit_Manipulation/20-sep-2024-- Swap Two Numbers.cpp
@@ -0,0 +1,16 @@
+
+/*
+Link:-
+https://www.naukri.com/code360/problems/swap-two-numbers_1112577?leftPanelTabValue=SUBMISSION
+*/
+
+void swapNumber(int &a, int &b) {
+	// Write your code here.
+
+	a=a^b;
+	b=a^b;
+	a=a^b;
+
+
+
+}
