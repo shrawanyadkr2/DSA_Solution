7c725d4ba6e7887f8eec77d6218faf43c999d593
shrawan kr. yadav <146319317+shrawanyadkr2@users.noreply.github.com>
2024-10-30 14:45:59 +0530

    Create 30-oct-2024--GFG.cpp

---
+++ POTD/30-oct-2024--GFG.cpp
@@ -0,0 +1,32 @@
+
+/*
+GFG
+
+https://www.geeksforgeeks.org/problems/pairs-with-difference-k1713/1
+
+Pairs with difference k
+
+
+
+
+*/
+
+
+class Solution {
+  public:
+    /* Returns count of pairs with difference k  */
+    int countPairsWithDiffK(vector<int>& arr, int k) {
+        // code here
+       int ans=0;
+       unordered_map<int,int>mp;
+       for(auto it:arr){
+           mp[it]++;
+       }
+       for(auto it:arr){
+           if(mp[it+k]>0){
+               ans+=mp[it+k];
+           }
+       }
+       return ans;
+    }
+};
