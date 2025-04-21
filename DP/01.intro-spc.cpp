a994d77bd646beba849de06cf8391b515b0e58ec
shrawan kr. yadav <146319317+shrawanyadkr2@users.noreply.github.com>
2025-04-21 07:12:53 +0530

    Create 01.intro-spc.cpp

---
+++ DP/01.intro-spc.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+// int fun(int n , vector<int>&dp){
+//   if()
+// }
+
+int main(){
+  int n=5;
+
+  int prev2=0;
+  int prev1=1;
+
+  for(int i=2; i<=n ;i++){
+    int curr=prev1+prev2;
+    prev2=prev1;
+    prev1=curr;
+  }
+  cout<<prev1;
+  return 0;
+}
